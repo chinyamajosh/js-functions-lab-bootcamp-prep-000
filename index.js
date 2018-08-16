967e10025b2b326e079a5f2c2e62fe3aca2d905b
Joshua Chinyama <chinyamajosh@gmail.com>
2018-08-16 09:46:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function happyHolidayTo(holiday, name) {
 
 happyHolidayTo('Christmas', "Joela")
 
-function holidayCountdown(holidays,days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} days until ${holiday}!`
 }
 
